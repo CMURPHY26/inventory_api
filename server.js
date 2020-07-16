8a7c104c63a4c3a8c8e9b07539367d70394fa37b
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-07-16 16:55:37 -0400

    added seed route with faker information

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 const PORT = process.env.PORT;
 const mongoose = require('mongoose');
 const productsController = require('./controllers/products.js');
+const Product = require('../models/product.js');
+const faker = require('faker');
+const { fake } = require('faker');
 
 // Error / Disconnection
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
@@ -21,10 +24,20 @@ app.use(express.json());//use .json(), not .urlencoded()
 app.use("/products", productsController);
 
 
+app.get("/products/seed", (req, res) => {
 
-
-
-
+    for(let i = 0; i < 10; i++) {
+        Product.create([
+            {
+                name: faker.commerce.productName(),
+                description: faker.commerce.product(),
+                item_type: faker.commerce.department(),
+                qty: faker.random.number(),
+                price: faker.commerce.price()
+            }
+        ])
+    }
+})
 
 app.listen(PORT, () => {
     console.log("Running on Port", PORT)
